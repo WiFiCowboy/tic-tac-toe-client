1e1e55e4d6f7fe43481087481760efb08cc90c97
Humberto Hernandez <hamburger45@gmail.com>
2020-04-01 20:52:56 -0700

    MVP complete

--- src/GameBoard/Game.js
+++ src/GameBoard/Game.js
@@ -2,6 +2,18 @@ import React from 'react'
 import Board from './Board'
 import TokenService from '../services/token-service'
 import './Game.css'
+import Square from './Square';
+
+const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
 class Game extends React.Component {
   constructor(props) {
@@ -19,16 +31,7 @@ class Game extends React.Component {
   }
 
   calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
+    //
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
@@ -44,7 +47,10 @@ class Game extends React.Component {
         game: winner === 'X' ? true : false
       }),
       method: 'post',
-      headers: { Authorization: `Bearer ${TokenService.getAuthToken()}` }
+      headers: {
+        Authorization: `Bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      }
     })
   }
 
@@ -74,13 +80,14 @@ class Game extends React.Component {
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
     const winner = this.calculateWinner(squares)
+    let endGame = this.state.endGame
     if (winner) {
       this.saveGame(winner)
-      this.setState({
-        endGame: true
-      })
+      endGame = true;
+
     }
     this.setState({
+      endGame,
       history: history.concat([
         {
           squares: squares
@@ -88,7 +95,71 @@ class Game extends React.Component {
       ]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    }, c => {
+      if (!this.state.xIsNext) {
+        this.computerPlayer(squares)
+      }
     });
+
+
+  }
+
+  computerPlayer(squares) {
+    const freePositions = []
+    squares.forEach((square, index) => {
+      if (!square) {
+        freePositions.push(index)
+      }
+    })
+    const computerPositions = []
+    squares.forEach((square, index) => {
+      if (square === 'O') {
+        computerPositions.push(index)
+      }
+    })
+    let position = Math.floor(Math.random() * freePositions.length)
+    let maxTotal = 0
+    let lineFound;
+    lines.forEach(line => {
+      let freeFound = false
+      let total = 0
+      line.forEach(pos => {
+        computerPositions.forEach(c => {
+          if (c === pos) {
+            total++
+          }
+        })
+
+        freePositions.forEach(c => {
+          if (c === pos) {
+            freeFound = true
+          }
+        })
+
+      })
+      if (total > maxTotal && freeFound) {
+        lineFound = line
+      }
+    })
+    if (lineFound) {
+      lineFound.forEach(l => {
+        let found = false;
+
+        freePositions.forEach(free => {
+          if (free === l) {
+            found = true
+          }
+        })
+        if (found === true) {
+          position = l
+        }
+
+      })
+
+    }
+    console.log(lineFound, position, freePositions);
+
+    this.handleClick(position)
   }
 
   jumpTo(step) {
