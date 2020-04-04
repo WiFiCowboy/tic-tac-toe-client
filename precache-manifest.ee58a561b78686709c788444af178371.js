f188b443235f62415bfb2ce03b17c2734729296a
Humberto Hernandez <hamburger45@gmail.com>
2020-04-03 22:48:38 -0700

    Updates

---
+++ precache-manifest.ee58a561b78686709c788444af178371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74ebe99da80beeac3157ad286b6a8ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fec020a98d96d932abc",
+    "url": "/static/css/main.c75fc401.chunk.css"
+  },
+  {
+    "revision": "2d492042187c0a882fbf",
+    "url": "/static/js/2.9e17bacd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.9e17bacd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fec020a98d96d932abc",
+    "url": "/static/js/main.50a737c7.chunk.js"
+  },
+  {
+    "revision": "82f7722cac10ecd55646",
+    "url": "/static/js/runtime-main.15075483.js"
+  }
+]);
