b6ea3c536297ee0eaf108d508de9fe99867076fd
Humberto Hernandez <hamburger45@gmail.com>
2020-04-04 13:10:10 -0700

    updated config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
   API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
-  // API_ENDPOINT: 'https://git.heroku.com/tic-tac-toe-server-capstone.git',
   TOKEN_KEY: 'thingful-client-auth-token',
 }
